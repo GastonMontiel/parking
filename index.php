89079591bbd6fabe349115df4fcc35afb74ee6f8
GastonMontiel <montielgaston95@gmail.com>
2023-09-02 15:33:18 -0300

    [AMETHYST]dev: style

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
 <?php 
-    if(isset($_POST['clr'])){ 
-        $selected_color= $_POST['clr'];
+    if(isset($_POST['color'])){ 
+        $selected_color= $_POST['color'];
 
         try {
             $dbh = new PDO('mysql:host=localhost;dbname=parking', 'root', '');
@@ -10,7 +10,7 @@
 
             try {
                 $dbh->beginTransaction();
-                $stmt->execute( array($_POST['clr']));
+                $stmt->execute( array($_POST['color']));
                 $dbh->commit();
             } catch(PDOExecption $e) {
                 $dbh->rollback();
@@ -78,40 +78,82 @@
      } catch( PDOExecption $e ) {
          print "Error!: " . $e->getMessage() . "</br>";
      }
+     
+    if(isset($_POST['modelName']) && isset($_POST['brandId'])) {
+        $stmt = $dbh->prepare("SELECT brandName FROM brands WHERE id=? LIMIT 1"); 
+        $stmt->execute([ $_POST['brandId']]); 
+        $brandNameAsociated = $stmt->fetch();
+    }
 ?>
 
-<div>
-    <form name="color_form" action="" method="post">
-         Crear un color: 
-            <?php if(isset($_POST['clr'])): ?>
-                <input name="clr" type="color" value="<?= $_POST['clr']?>">
-            <?php else: ?>
-                <input name="clr" type="color">
-            <?php endif; ?>
-         <input type="submit" name="submit">
-     </form>
 
-     <form name="brandName_form" action="" method="post">
-         Crear una marca: 
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet"> 
+        <title>PARKING</title>
+    </head>
+<body>
+<div class="container-form">
+    <form name="color_form" class="" action="" method="post">
+        <p class="mb-3 fs-1">Crear un color</p>
+        
+        <div class="mb-2 flex flex-column">
+        <label class="" for="input-color">Selecciona un color:</label> 
+            <div class="flex  mt-2 items-center">
+                <input name="color" class="unstyle " id="input-color" required type="color">
+                <input type="submit" class="button-submit submit-color" value="Crear color" name="submit">
+            </div>
+        </div>
+        <div class="result">
+            <?php if(isset($_POST['color'])): ?>
+                <div class="flex">
+                    <p>Se creo el color: </p> <div class="div-circle" style="background: <?= $_POST['color']?>;"></div>
+                </div>
+            <?php endif ?>
+        </div>
+    </form>
+    <hr>    
+     <form name="brandName_form" class="" action="" method="post">
+     <p class="mb-3 fs-1">Crear una marca</p>
+        <div class="mb-2 flex flex-column"> 
+            <label class="" for="input-brand-name">ingrese una marca:</label>
+            <input name="brandName" id="input-brand-name" class="mb-2 mt-2" required type="text">
+            <input type="submit" class="button-submit input-radius" name="submit">
+        </div>
+        <div class="result">
             <?php if(isset($_POST['brandName'])): ?>
-                <input name="brandName" disabled type="text" value="<?= $_POST['brandName']?>">
-            <?php else: ?>
-                <input name="brandName" type="text">
-            <?php endif; ?>
-         <input type="submit" name="submit">
+                <div class="flex">
+                    <p>Se creo la marca: <?= $_POST['brandName']?></p>
+                </div>
+            <?php endif ?>
+        </div>
      </form>
+     <hr>    
+     <form name="model_to_brand_form" class="" action="" method="post">
+            <p class="mb-3 fs-1">Crear un modelo para una marca</p>
 
-     <form name="model_to_brand_form" action="" method="post">
-         Crear una modelo para una marca: 
+                
+          
             <?php if(isset($_POST['modelName'])): ?>
-                <input name="modelName" type="text" value="<?= $_POST['modelName']?>">
-                <input name="brandId" type="text" value="<?= $_POST['brandName']?>">
-                //hacer el join para obetener la marca a la qeu se asocio el modelo
+                <div class="result">
+                    <?php if(isset($_POST['modelName'])): ?>
+                        <div class="flex">
+                            <p>Se creo el modelo:  <strong><?= $_POST['modelName']?></strong> para: <?= $brandNameAsociated['brandName'] ?> </p>
+                            
+                        </div>
+                    <?php endif ?>
+                </div>
             <?php else: ?>
 
-                <div>
+                <div class="mb-3 flex flex-column"> 
                     <label for="brandSelect">seleccione una marca:</label>
-                    <select required name="brandId" id="brandSelect">
+                    <select required name="brandId" class="mt-2" id="brandSelect">
                         <?php foreach($allBrands as $key ){ ?>
                             <option  value="<?= $key['id'];?>"> <?= $key['brandName'];?></option> 
 
@@ -119,12 +161,146 @@
                     </select>
                 </div>
 
-                <div>
+                <div class="mb-2 flex flex-column"> 
                     <label for="modelName">ingrese un modelo:</label> 
-                    <input name="modelName" required id="modelName" type="text">
+                    <input name="modelName" required id="modelName" class="mt-2 mb-2" type="text">
+                    <input type="submit" class="button-submit input-radius" name="submit">
                 </div>
             <?php endif; ?>
-         <input type="submit" name="submit">
      </form>
-
 </div>
+
+
+<style>
+    .flex {
+        display: flex;
+    }
+
+    .flex-column {
+        flex-direction: column;
+    }
+
+    .justify-center {
+        justify-content: center;
+    }
+
+    .justify-between {
+        justify-content: space-between;
+    }
+
+    .items-center {
+        align-items: center;
+    }
+
+    .mb-2{
+        margin-bottom: 10px
+    }
+
+    .mb-3{
+        margin-bottom: 15px
+    }
+
+    .mt-2{
+        margin-top: 10px
+    }
+
+    .fs-1 {
+        font-size: 1.5rem ;
+    }
+
+    .fs-2 {
+        font-size: 1.3rem ;
+    }
+
+    .fs-3 {
+        font-size: 1.2rem ;
+    }
+
+    #input-color {
+        height: 34px;
+        width: 80px;
+        height: 40px;
+    }
+
+    #input-color::-webkit-color-swatch-wrapper {
+        padding: 0; 
+    }
+
+    #input-color::-webkit-color-swatch {
+        border: none;
+        border-radius: 20px 0 0 20px;
+    }
+
+    .submit-color{
+        margin-left: -3px;
+        height: 40px;
+        border-radius: 0 4px 4px 0;
+    }
+
+    input[type=color] {
+        border:none;
+        background-image:none;
+        background-color:transparent;
+        -webkit-box-shadow: none;
+        -moz-box-shadow: none;
+        box-shadow: none;
+    }
+
+    input[type=submit] {
+        background: #b6e3ff;
+        border: solid 1px #0a49bd;
+    }
+
+    input[type=text], input[type=submit],  select {
+        height: 40px;
+    }
+
+    .unstyle {
+     all: unset;
+    }
+
+    .container-all-forms {
+        width: fit-content;
+        padding: 15px;
+        border-radius: 8px;
+        border: solid 1px black;
+    }
+
+    body {
+        display: flex;
+        justify-content: center;
+        font-family: 'Inter', sans-serif;
+    }
+
+    .result {
+        height: 18px;
+    }
+
+    p {
+        margin: 0px;
+    }
+
+    .input-radius { 
+        border-radius: 4px;
+    }
+
+    .div-circle {
+        border-radius: 50px;
+        height: 18px;
+        width: 18px;
+        margin-left: 18px;
+    }
+
+    .container-form {
+        padding: 10px;
+        border: solid 1px gray;
+        border-radius: 4px;
+    }
+
+</style>
+
+
+
+</body>
+</html>
+
